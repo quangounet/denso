7a4ef7b6206198db0e8e327207aba248cbf01747
quangounet <cuong.pham@normalesup.org>
2014-04-08 14:41:50 +0800

    test gain modulation

--- python/script_optim.py
+++ python/script_optim.py
@@ -1,3 +1,6 @@
+#cd "C:\Documents and Settings\RRC Staff\git\denso\python"
+#cd git/denso/python/
+
 from pylab import *
 from numpy import *
 import Trajectory
@@ -5,8 +8,6 @@ import denso
 
 
 ion()
-
-#cd git/denso/python/
 
 
 vmax = array([ 3.92699082,  2.61799388,  2.85832572,  3.92699082,  3.02168853,
@@ -26,17 +27,20 @@ cpos = 10000
 cvel = 10
 cacc = 0.01
 cdur = 0.01
-gainoptim = True
+gainoptim = False
 xopt = denso.FindOptTraj(trajref,nwaypoints,nsamples,[cpos,cvel,cacc,cdur],vmax,amax,gainoptim)
 ndof = trajref.dimension
 qstart = trajref.Eval(0)
 qend = trajref.Eval(trajref.duration)
-trajopt = denso.MakeTraj(xopt,qstart,qend,ndof,nwaypoints,nsamples,vmax,amax)
+trajopt = denso.MakeTraj(xopt,qstart,qend,ndof,nwaypoints,vmax,amax)
 close('all')
 denso.PlotKinematics(trajref.Retime(trajopt.duration/trajref.duration),trajopt,dt=0.001,colorcycle=['r','g','b'],tstart=0)
 
 # Write the optimal waypoints into a pcs file
 nextracols = 3
-#denso.CreateProgram(xopt,trajref,nwaypoints,"../data/constraintparabolicsmoothingopt.pcs",nextracols)
+qlist,vcoeflist,acoeflist = denso.ListFromVector(xopt,trajref.dimension,nwaypoints)
+qlist.insert(0,trajref.Eval(0))
+qlist.append(trajref.Eval(trajref.duration))
+#denso.CreateProgram(qlist,"../data/constraintparabolicsmoothingopt.pcs",vcoeflist=vcoeflist,acoeflist=acoeflist,nextracols=nextracols)
 #open("../data/constraintparabolicsmoothingopt.topptraj","w").write(str(trajopt))
 
