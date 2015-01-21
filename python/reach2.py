4e64425f9e15c5b6b3cfa666c93a442efee8170e
Puttichai <L.Puttichai@gmail.com>
2015-01-21 16:06:46 +0800

    improve procedure in reach2.py

--- python/reach2.py
+++ python/reach2.py
@@ -11,6 +11,7 @@ from TOPP import Trajectory
 import time
 import string
 import numpy as np
+import copy
 
 """
 reach2.py
@@ -60,17 +61,14 @@ box5.SetTransform(np.array([[  1.0,  0.0,  0.0,  0.00],
                             [  0.0,  0.0,  1.0,  0.800], 
                             [  0.0,  0.0,  0.0,  1.00]]))
 
+box3.Enable(False)
+box3.SetVisible(False)
 box4.Enable(False)
 box4.SetVisible(False)
 
 H = np.array([[0, 1, 0, 0], [-1, 0, 0, 0], [0, 0, 1, 0.59], [0, 0, 0, 1]])
 robot.SetTransform(H)
 bottle.SetTransform(H)
-
-# H_rotation = np.array([[ 0,  1,  0,  0], 
-#                        [-1,  0,  0,  0], 
-#                        [ 0,  0,  1,  0], 
-#                        [ 0,  0,  0,  1]])
 
 H_rotation = np.eye(4)
 
@@ -91,44 +89,77 @@ with open(filedirectory, 'r') as f:
     matrixstring = f.read()
 
 y = [float(x) for x in matrixstring.split()]
-# transformation matrix from camera's frame to the calibration frame
+## transformation matrix from camera's frame to the calibration frame
 H_ref2realpos = np.array([[y[0], y[1], y[2], y[3]], 
                           [y[4], y[5], y[6], y[7]], 
                           [y[8], y[9], y[10], y[11]], 
                           [0.0, 0.0, 0.0, 1.0]])
 
-H_res = np.dot(H_2ref, H_ref2realpos)
+H_res = np.dot(H_2ref, H_ref2realpos) ## the resulting transformation of the bottle
 bottle.SetTransform(H_res)
 
-init_config = np.array([  0.00000000e+00,   9.12246826e-01,   6.39601469e-01,
+init_config = np.array([  2.40000000e-01,   9.12246826e-01,   6.39601469e-01,
                           4.63102048e-16,  -1.34505559e+00,   8.51749227e-16])
 robot.SetDOFValues(init_config)
+
 # bottle.Enable(False)
+trayoffset = []
+trayoffset.append(np.array([[1.0,  0.0,  0.0,  0.05],
+                            [0.0,  1.0,  0.0,  0.05],
+                            [0.0,  0.0,  1.0, -0.026],
+                            [0.0,  0.0,  0.0,  1.0]]))
 
-H_goal = H_res
-H_goal[2][3] += 0.032# -= 0.016 # offset (the height of the tray)
+trayoffset.append(np.array([[1.0,  0.0,  0.0,  0.10],
+                            [0.0,  1.0,  0.0,  0.05],
+                            [0.0,  0.0,  1.0, -0.026],
+                            [0.0,  0.0,  0.0,  1.0]]))
 
-manip = robot.SetActiveManipulator('Flange') # set the manipulator to the flange (end-effector)
-ikmodel = databases.inversekinematics.InverseKinematicsModel(robot, iktype = IkParameterization.Type.Transform6D)
-if not ikmodel.load():
-    ikmodel.autogenerate()
+trayoffset.append(np.array([[1.0,  0.0,  0.0,  0.00],
+                            [0.0,  1.0,  0.0,  0.05],
+                            [0.0,  0.0,  1.0, -0.026],
+                            [0.0,  0.0,  0.0,  1.0]]))
 
-with env: # lock environment
-    sol = manip.FindIKSolution(H_goal, IkFilterOptions.CheckEnvCollisions) # get collision-free solution
+trayoffset.append(np.array([[1.0,  0.0,  0.0,  0.05],
+                            [0.0,  1.0,  0.0,  0.10],
+                            [0.0,  0.0,  1.0, -0.026],
+                            [0.0,  0.0,  0.0,  1.0]]))
 
-if (sol == None):
-    print "No inverse kinematic solution found."
-else:
-    maxiter = 1000
-    robot.SetDOFValues(init_config)
-    with robot:
+trayoffset.append(np.array([[1.0,  0.0,  0.0,  0.05],
+                            [0.0,  1.0,  0.0,  0.00],
+                            [0.0,  0.0,  1.0, -0.026],
+                            [0.0,  0.0,  0.0,  1.0]]))
+
+withsol = False
+for T in trayoffset:
+    
+    H_goal = np.dot(H_res, T) ## goal transformation for end-effector
+    
+    manip = robot.SetActiveManipulator('Flange') ## set the manipulator to the flange (end-effector)
+    ikmodel = databases.inversekinematics.InverseKinematicsModel(robot, iktype = IkParameterization.Type.Transform6D)
+    if not ikmodel.load():
+        ikmodel.autogenerate()
+
+    with env: ## lock environment
+        sol = manip.FindIKSolution(H_goal, IkFilterOptions.CheckEnvCollisions) ## get collision-free solution
+
+    if (sol == None):
+        print "No inverse kinematic solution found."
+        continue
+    else:
+        maxshortcutiter = 500
+        robot.SetDOFValues(init_config)
+    
         params = Planner.PlannerParameters()
         params.SetRobotActiveJoints(robot)
         params.SetGoalConfig(sol)
-        extraparams = '<_postprocessing planner="' + 'parabolic' + 'smoother' + '">'
-        extraparams += '<_nmaxiterations>' + str(int(maxiter)) + '</_nmaxiterations></_postprocessing>'
+        
+        extraparams = '<_postprocessing></_postprocessing>'
         params.SetExtraParameters(extraparams)
-            
+        
+        extraparams = '<_postprocessing planner="' + 'parabolic' + 'smoother' + '">'
+        extraparams += '<_nmaxiterations>' + str(int(maxshortcutiter)) + '</_nmaxiterations></_postprocessing>'
+        params.SetExtraParameters(extraparams)
+        
         planner = RaveCreatePlanner(env, 'birrt')
         planner.InitPlan(robot, params)
         
@@ -136,6 +167,17 @@ else:
         status = planner.PlanPath(ravetraj)
         if (status == 1 or status == 3):
             TOPPtraj = TOPPopenravepy.FromRaveTraj(robot, ravetraj)
+            traj = TOPPopenravepy.FromRaveTraj(robot, ravetraj)
+            with open('../data/movetobottle.traj', 'w') as f:
+                f.write(str(traj))
+            withsol = True
+            break
         else:
             print "No trajectory found."
-        
+            continue
+   
+print "\n**********"
+if withsol:
+    print "Successfully computed a trajectory"
+else:
+    print "Failed"
